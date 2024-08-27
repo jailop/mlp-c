a3c12bad50fd00e501e558a7f3864d490fba33bd
Jaime Lopez <jailop@protonamil.com>
2024-08-27 11:14:44 -0400

    Update

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Multilayer Perceptron in C
 
-A multilayer perceptron code wrote in ANSI C
+A multilayer perceptron code wrote in ANSI C.
+
+Check the maths: <https://datainquiry.dev/blog/2021/basic-neural-network.pdf>
 
 This project was moved from [Data Science Plays/mlp-c](https://gitlab.com/jailop/data-science-plays/-/tree/master/mlp-c?ref_type=heads).
--- neuralnet.h
+++ neuralnet.h
@@ -10,20 +10,42 @@ typedef struct {
   matrix_t **b;
   matrix_t **z;
   matrix_t **a;
-  double (*actfunc)(double);
-  double (*d_actfunc)(double);
+  double (*actfunc)(double);   // activation function
+  double (*d_actfunc)(double); // activation function derivative
 } neuralnet_t;
 
 double sigmoid(double x);
 double d_sigmoid(double x);
 
-neuralnet_t *neuralnet_new(int *n_nodes, int n_layers, 
-  double (*actfunc)(double), double (*d_actfunc)(double));
+/**
+ * neuralnet_new creates a new neuralnet object
+ *
+ * Arguments:
+ *   n_nodes   : Array with number of nodes by layer
+ *   n_layers  : Number of layers
+ *   actfunc   : Activation function
+ *   d_actfunc : Activation function derivative
+ *
+ * Returns:
+ *   neuralnet_t allocated object
+ */
+neuralnet_t *neuralnet_new(
+    int *n_nodes,
+    int n_layers, 
+    double (*actfunc)(double),
+    double (*d_actfunc)(double)
+);
+
 void neuralnet_free(neuralnet_t *nn);
+
 void neuralnet_forward(neuralnet_t *nn, matrix_t *X);
+
 void neuralnet_backpropagation(neuralnet_t *nn, matrix_t *X, matrix_t *Y, double nabla);
+
 void neuralnet_fit(neuralnet_t *nn, matrix_t *X, matrix_t *Y, int epochs, double nabla);
+
 matrix_t *neuralnet_predict(neuralnet_t *nn, matrix_t *X);
 
 matrix_t *model_error(matrix_t *Y, matrix_t *Y_hat);
+
 #endif /* _NEURALNET_H */
